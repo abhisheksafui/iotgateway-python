5cb9bf463321d994c9757b93c030b7d51435e0e5
ABHISHEK SAFUI <abhishek6590@gmail.com>
2020-07-30 00:00:26 +0530

    methods added to gateway

--- src/gateway.py
+++ src/gateway.py
@@ -1,5 +1,6 @@
 from awscrt import io, mqtt
 from awsiot import iotshadow, mqtt_connection_builder
+import logging
 
 
 class IotGateway:
@@ -18,7 +19,7 @@ class IotGateway:
         host_resolver = io.DefaultHostResolver(event_loop_group)
         client_bootstrap = io.ClientBootstrap(event_loop_group, host_resolver)
 
-        mqtt_connection = mqtt_connection_builder.mtls_from_path(
+        self.mqtt_connection = mqtt_connection_builder.mtls_from_path(
             endpoint=self.endpoint,
             cert_filepath=self.certificate_path,
             pri_key_filepath=self.private_key_path,
@@ -28,6 +29,33 @@ class IotGateway:
             clean_session=False,
             keep_alive_secs=6)
 
-        connected_future = mqtt_connection.connect()
-        self.shadow_client = iotshadow.IotShadowClient(mqtt_connection)
+        connected_future = self.mqtt_connection.connect()
+        self.shadow_client = iotshadow.IotShadowClient(self.mqtt_connection)
         connected_future.result()
+
+        logging.info("Connected to mqtt server.")
+
+
+    def subscribe_device_topics(self):
+
+        self_topic = "/" + self.thing_name
+        self.subscribe(self_topic, self.on_message)
+
+    def on_message(self, topic, payload, **kwargs):
+        
+        return
+    def subscribe(self, topic, callback):
+
+        logging.info("Subscribing to topic: {topic}".format(topic=topic))
+        try:
+
+            subscribe_future, _ = self.mqtt_connection.subscribe(
+                topic=topic,
+                qos=mqtt.QoS.AT_LEAST_ONCE,
+                callback=callback)
+
+            subscribe_result = subscribe_future.result()
+            logging.info("Subscribed with {}".format(str(subscribe_result['qos'])))
+        except Exception as e:
+            logging.info(e)
+
